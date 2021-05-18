bc6b786cbb8ac069b82cc48b48cc93fdd15e41fa
ElianaCorbalan <eli_corbalan@hotmail.com.ar>
2021-05-18 11:41:14 -0300

    subiendo archivos

---
+++ src/views/Search/components/SearchResults/index.js
@@ -0,0 +1,23 @@
+export default function SearchResults({ results, isSearching }){
+    return (
+        <div style={{
+            width:"100%",
+            padding:"0rem 2rem 0rem 2rem",
+            }}>
+
+            {!results?.length && isSearching && <p>No existen resultados</p>}    
+            {results?.map((value) => {
+                return (
+                    <div key={value.id} style={{ 
+                        backgroundColor: "#B5B7B7", 
+                        marginTop:"1rem", marginBottom:"1rem", 
+                        padding: 10, width:"100%"
+                        }}>
+                        <p>{value.name}</p>
+                        <p>{value.email}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
